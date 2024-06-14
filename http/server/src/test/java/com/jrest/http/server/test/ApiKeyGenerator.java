e9093a66737c6624c42d8812cbe3c809f2cd3fae
xxcoldinme <misha_stonlex@bk.ru>
2024-06-14 23:03:43 +0300

    http-server: add AuthKeyGenerator

--- http/server/src/test/java/com/jrest/http/server/test/ApiKeyGenerator.java
+++ http/server/src/test/java/com/jrest/http/server/test/ApiKeyGenerator.java
@@ -1,20 +1,10 @@
 package com.jrest.http.server.test;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
+import com.jrest.http.server.apikey.AuthKeyGenerator;
 
 public class ApiKeyGenerator {
-    private static final int KEY_LENGTH = 32; // Длина ключа в байтах
-
-    public static String generateApiKey() {
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] apiKeyBytes = new byte[KEY_LENGTH];
-        secureRandom.nextBytes(apiKeyBytes);
-        return new BigInteger(1, apiKeyBytes).toString(16);
-    }
 
     public static void main(String[] args) {
-        String apiKey = generateApiKey();
-        System.out.println("Сгенерированный API-ключ: " + apiKey);
+        System.out.println("Сгенерированный API-ключ: " + AuthKeyGenerator.defaults(20).generate());
     }
 }
